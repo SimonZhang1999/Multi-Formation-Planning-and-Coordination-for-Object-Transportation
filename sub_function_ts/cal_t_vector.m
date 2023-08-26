86c51e32d2fbbae7a9b6dd66694e4498159e1e8c
Weijian Zhang <93252979+SimonZhang1999@users.noreply.github.com>
2023-08-26 12:59:50 +0100

    Delete sub_function_ts directory

--- sub_function_ts/cal_t_vector.m
+++
@@ -1,8 +0,0 @@
-%% Compute time vector
-%% n=0:position n=1:velocity n=2:acceleration n=3:jerk
-function t_vec = cal_t_vector(t,n_order,n)
-    t_vec = zeros(1,n_order+1);
-    for i= n+1:n_order+1
-        t_vec(i) = prod(i-n:i-1)*t^(i-n-1);
-    end
-end
--- sub_function_ts/cal_time_stamp.m
+++
@@ -1,5 +0,0 @@
-function ts = cal_time_stamp(inserted_traj,T)
-    dis = sum((inserted_traj(:,2:end) - inserted_traj(:,1:end-1)).^2,1).^0.5;
-    dt = T/sum(dis);
-    ts = [0 cumsum(dis*dt)];
-end
